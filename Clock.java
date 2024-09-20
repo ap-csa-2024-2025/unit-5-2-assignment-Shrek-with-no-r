a41d87b9fd4b066c19867ea7cad348020bc9b10c
shrek-with-no-r <cmsarr100@student.hbuhsd.edu>
2024-09-20 11:21:40 -0700

    jkkj

--- Clock.java
+++ Clock.java
@@ -2,9 +2,29 @@ public class Clock
 {
   public static void main(String[] args)
   {
-    // test code here
+    clock(4);
   }
 
-  // write code for timeOfDay here
+  public static int clock(int x)
+  {
+    if (x != 0)
+    {
+      if (x < 12)
+      {
+        System.out.println("It is currently morning");
+      }
+      else if (x < 18)
+      {
+        System.out.println("It is currently the afternoon");
+      }
+      else if (x <= 24)
+      {
+        System.out.println("It is currently evening");
+      }
+    }else 
+    {
+      System.out.println("It is currently midnight");Object u;
+    }
+  }
 
 }
